237175d18b67ba62ce03e538b40ab25d7d387e97
dang <trinhdangnhat@gmail.com>
2022-08-19 18:08:39 +0200

    validation abstract

--- build/app.js
+++ build/app.js
@@ -29,7 +29,37 @@ class ProjectInput {
     }
     submitHandler(event) {
         event.preventDefault();
+        this.cleanError();
+        const newProject = new Project(this.titleInputElement.value, this.descriptionInputElement.value, +this.peopleInputElement.value);
+        const validator = new ValidateExecutor();
+        const resultValidation = validator.validate(newProject);
+        console.log(resultValidation);
+        if (!validator.isValidateGetter) {
+            this.showErrorInput(resultValidation);
+            return;
+        }
         this.clearInputs();
+        console.log("submit ok");
+    }
+    cleanError() {
+        const nodeList = this.element.querySelectorAll(".error");
+        for (let i = 0; i < nodeList.length; i++) {
+            nodeList[i].remove();
+        }
+    }
+    showErrorInput(obj) {
+        for (const [key] of Object.entries(obj)) {
+            const mappingInput = {
+                title: this.titleInputElement,
+                description: this.descriptionInputElement,
+                people: this.peopleInputElement,
+            };
+            if (obj[key][0] === false) {
+                const templateErr = `<div class="error">${obj[key][1]}</div>`;
+                mappingInput[key].focus();
+                mappingInput[key].insertAdjacentHTML("afterend", templateErr);
+            }
+        }
     }
     configure() {
         this.element.addEventListener("submit", this.submitHandler);
@@ -46,5 +76,84 @@ class ProjectInput {
 __decorate([
     autobind
 ], ProjectInput.prototype, "submitHandler", null);
-new ProjectInput();
+const projectInput = new ProjectInput();
+function Required() {
+    return function (target, propName) {
+        var _a;
+        registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { [propName]: Object.assign(Object.assign({}, (registeredValidators[target.constructor.name] ? (_a = registeredValidators[target.constructor.name][propName]) !== null && _a !== void 0 ? _a : {} : {})), { required: true }) });
+        console.log(registeredValidators);
+    };
+}
+function Min(min) {
+    return function (target, propName) {
+        var _a;
+        registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { [propName]: Object.assign(Object.assign({}, (registeredValidators[target.constructor.name] ? (_a = registeredValidators[target.constructor.name][propName]) !== null && _a !== void 0 ? _a : {} : {})), { min: min }) });
+    };
+}
+function MinLength(sizeMin) {
+    return function (target, propName) {
+        var _a;
+        registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { [propName]: Object.assign(Object.assign({}, (registeredValidators[target.constructor.name] ? (_a = registeredValidators[target.constructor.name][propName]) !== null && _a !== void 0 ? _a : {} : {})), { minLength: sizeMin }) });
+    };
+}
+const registeredValidators = {};
+class Project {
+    constructor(title, description, people) {
+        this.title = title;
+        this.people = people;
+        this.description = description;
+    }
+}
+__decorate([
+    Required()
+], Project.prototype, "title", void 0);
+__decorate([
+    Min(2),
+    Required()
+], Project.prototype, "people", void 0);
+__decorate([
+    MinLength(5),
+    Required()
+], Project.prototype, "description", void 0);
+class ValidateExecutor {
+    constructor() {
+        this._isValidate = true;
+    }
+    set isValidateSetter(value) {
+        this._isValidate = value;
+    }
+    get isValidateGetter() {
+        return this._isValidate;
+    }
+    validate(obj) {
+        const objValidatorConfig = registeredValidators[obj.constructor.name];
+        const resultValidation = {};
+        for (const prop in objValidatorConfig) {
+            const allConditionValidation = objValidatorConfig[prop];
+            for (const [key, value] of Object.entries(allConditionValidation)) {
+                const res = this[`${key}`](obj[prop], value);
+                console.log("thu tu goi function:", prop);
+                if (res[0] === false) {
+                    this.isValidateSetter = false;
+                    resultValidation[prop] = res;
+                    break;
+                }
+                resultValidation[prop] = [true, ""];
+            }
+        }
+        console.log("bool:", this.isValidateGetter);
+        return resultValidation;
+    }
+    required(input, val, msg) {
+        if (val === false)
+            return [true, ""];
+        return [!(input.toString().trim().length === 0), msg !== null && msg !== void 0 ? msg : "This field is required"];
+    }
+    min(input, min, msg) {
+        return [input > min, msg !== null && msg !== void 0 ? msg : "Number must be more than " + min + "."];
+    }
+    minLength(input, min, msg) {
+        return [input.toString().trim().length > min, msg !== null && msg !== void 0 ? msg : "text must have more than " + min + " characters"];
+    }
+}
 //# sourceMappingURL=app.js.map
--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,4 +1,3 @@
-// autobind decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
   const adjDescriptor: PropertyDescriptor = {
@@ -38,8 +37,45 @@ class ProjectInput {
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
+    this.cleanError();
+    const newProject = new Project(this.titleInputElement.value, this.descriptionInputElement.value, +this.peopleInputElement.value);
+
+    const validator = new ValidateExecutor();
+    const resultValidation = validator.validate(newProject);
+
+    console.log(resultValidation);
+    if (!validator.isValidateGetter) {
+      // show error & focus
+      this.showErrorInput(resultValidation);
+
+      return;
+    }
 
     this.clearInputs();
+
+    console.log("submit ok");
+  }
+
+  private cleanError() {
+    const nodeList = this.element.querySelectorAll(".error");
+    for (let i = 0; i < nodeList.length; i++) {
+      nodeList[i].remove();
+    }
+  }
+  private showErrorInput(obj: IResultValitation) {
+    for (const [key] of Object.entries(obj)) {
+      const mappingInput = {
+        title: this.titleInputElement,
+        description: this.descriptionInputElement,
+        people: this.peopleInputElement,
+      };
+
+      if (obj[key][0] === false) {
+        const templateErr = `<div class="error">${obj[key][1]}</div>`;
+        (<any>mappingInput)[key].focus();
+        (<any>mappingInput)[key].insertAdjacentHTML("afterend", templateErr); //
+      }
+    }
   }
 
   //set up event listener to form
@@ -58,4 +94,142 @@ class ProjectInput {
   }
 }
 
-new ProjectInput();
+const projectInput = new ProjectInput();
+
+interface Validator {
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+/*
+interface IProject {
+  title: string;
+  people: number;
+  description: string;
+}
+*/
+
+type tupleMsgValidation = [boolean, string];
+type IResultValitation = {
+  // {title:[true,"something"]}
+  [nameProp: string]: tupleMsgValidation;
+};
+
+type IRegistorValidator = {
+  [nameClass: string]: { [props: string]: Validator };
+};
+
+// Decorators
+
+function Required() {
+  return function (target: any, propName: string) {
+    registeredValidators[target.constructor.name] = {
+      ...registeredValidators[target.constructor.name],
+
+      [propName]: { ...(registeredValidators[target.constructor.name] ? registeredValidators[target.constructor.name][propName] ?? {} : {}), required: true },
+    };
+
+    console.log(registeredValidators);
+  };
+}
+
+function Min(min: number) {
+  return function (target: any, propName: string) {
+    registeredValidators[target.constructor.name] = {
+      ...registeredValidators[target.constructor.name],
+      [propName]: { ...(registeredValidators[target.constructor.name] ? registeredValidators[target.constructor.name][propName] ?? {} : {}), min: min },
+    };
+  };
+}
+
+function MinLength(sizeMin: number) {
+  return function (target: any, propName: string) {
+    registeredValidators[target.constructor.name] = {
+      ...registeredValidators[target.constructor.name],
+      [propName]: { ...(registeredValidators[target.constructor.name] ? registeredValidators[target.constructor.name][propName] ?? {} : {}), minLength: sizeMin },
+    };
+  };
+}
+
+//TODO: get after decorator implement
+//const registeredValidators: IRegistorValidator = { Project: { title: { required: true }, description: { required: true, minNumber: 5 } } };
+const registeredValidators: IRegistorValidator = {}; //init empty object
+
+class Project {
+  @Required()
+  title: string;
+
+  @Min(2)
+  @Required()
+  people: number;
+
+  @MinLength(5)
+  @Required()
+  description: string;
+
+  constructor(title: string, description: string, people: number) {
+    this.title = title;
+    this.people = people;
+    this.description = description;
+  }
+}
+
+class ValidateExecutor {
+  private _isValidate: boolean = true;
+
+  constructor() {}
+
+  set isValidateSetter(value: boolean) {
+    this._isValidate = value;
+  }
+
+  get isValidateGetter(): boolean {
+    return this._isValidate;
+  }
+
+  validate(obj: any): IResultValitation {
+    const objValidatorConfig = registeredValidators[obj.constructor.name];
+
+    // check if (Object.keys({}).length === 0) {
+    //console.log('I will not print');
+    // }
+
+    const resultValidation: IResultValitation = {}; // {title:[false,"somethingwrong"]}
+    for (const prop in objValidatorConfig) {
+      const allConditionValidation: Validator = objValidatorConfig[prop]; // {required: true, minNumber: 5 }
+
+      for (const [key, value] of Object.entries(allConditionValidation)) {
+        //console.log(prop, key, value); //title required true
+
+        const res = (<any>this)[`${key}`](obj[prop], value); // call function by string name
+        console.log("thu tu goi function:", prop);
+        if (res[0] === false) {
+          this.isValidateSetter = false;
+          resultValidation[prop] = res;
+          break; // exit loop inner to get only first false condition
+        }
+        resultValidation[prop] = [true, ""]; //will be override but its ok
+      }
+    }
+
+    console.log("bool:", this.isValidateGetter);
+
+    return resultValidation;
+  }
+
+  required(input: string, val: boolean, msg?: string): tupleMsgValidation {
+    if (val === false) return [true, ""]; // do nothing
+    return [!(input.toString().trim().length === 0), msg ?? "This field is required"];
+  }
+
+  min(input: number, min: number, msg?: string): tupleMsgValidation {
+    return [input > min, msg ?? "Number must be more than " + min + "."];
+  }
+
+  minLength(input: string, min: number, msg?: string): tupleMsgValidation {
+    return [input.toString().trim().length > min, msg ?? "text must have more than " + min + " characters"];
+  }
+}
